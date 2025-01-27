0e35ee02c865618efcc8b06d837180344e590d0c
Naima Akhtar <naimasanaakhtar@gmail.com>
2025-01-27 17:58:51 +0100

    ajout de code dans details.php

--- templates/details.php
+++ templates/details.php
@@ -6,7 +6,30 @@ link_to_css('static/details.css');?>
 <body>
     <?php include('global/header.php'); ?>
     <main>
-        
+        <section class="titre">
+            <h1>Détails du restaurant ...</h1>
+        </section>
+        <section>
+            <div class="image_nom">
+                <div class="image">
+                </div>
+                <div class="nom">
+                </div>
+            </div>
+            <div class="details_lien">
+                <div class="details">
+                    <p>Adresse : </p>
+                    <p>Origine : </p>
+                    <p>Inclus : </p>
+                    <p>Horaire : </p>
+                    <p>Email : </p>
+                    <p>Numéro : </p>
+                </div>
+                <div>
+                    <a href="">Inscrivez vous dès maintenant pour voir les avis de ce restaurant !</a>
+                </div>
+            </div>
+        </section>
     </main>
     <?php include('global/footer.php'); ?>
 </body>
