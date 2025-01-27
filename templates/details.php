604d24e079ae5e5b9ed74bb64fb736ab28210bb6
Naima Akhtar <naimasanaakhtar@gmail.com>
2025-01-27 17:44:41 +0100

    ajout des fichiers details

---
+++ templates/details.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+<?php include('global/head.php'); 
+title_html('Détails');
+link_to_css('static/details.css');?>
+<body>
+    <?php include('global/header.php'); ?>
+    <main>
+        
+    </main>
+    <?php include('global/footer.php'); ?>
+</body>
+</html>
