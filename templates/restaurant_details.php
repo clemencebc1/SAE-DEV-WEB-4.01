6261cd2bea0fb6700dd0c9035ad8c6f344a185c4
Naima <naimasanaakhtar@gmail.com>
2025-03-05 11:49:10 +0100

    modification render restaurant

--- templates/restaurant_details.php
+++ templates/restaurant_details.php
@@ -42,7 +42,7 @@ link_to_css('static/details.css');?>
     $restaurant_render = new Restaurant_render([$restaurant]);
     $restaurant_render->render();
 
-    if (count($_SESSION)>=1){
+    if (count($_SESSION['user'])){
         $favoris = DBConnector::getFavorisByUser($_SESSION['user']['username']);
         $inFavoris = false;
         foreach ($favoris as $favori){
@@ -60,9 +60,13 @@ link_to_css('static/details.css');?>
         echo "<input type='hidden' name='id' value='" . $_GET['id'] . "'>";
         echo "<button type='submit' class='svg-heart-btn'><svg viewBox='0 0 24 24' width='40' height='40' fill='". $fill."'><path d='M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z'/></svg></button></form>";
         echo "</form>";
+
+        echo "<button class='critique' style='margin-top: 10px;'>Voir les critiques de ce restaurant</button>";
+        echo "<div id='reviews-container' style='display: none;'>";
         $critiques = DBConnector::getCritiqueByRestaurant(intval($_GET['id']));
         $renderCritique = new CritiqueRender($critiques);
         $renderCritique->render_critiques_restaurant();
+        echo "</div>";
     }
     ?>
     </main>
